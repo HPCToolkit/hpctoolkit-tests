87dc74860c5d0deb86e5e73a982dd80bdae3ef74
John Mellor-Crummey <johnmc@rice.edu>
2018-03-21 01:17:58 -0500

    hpcprof/viewer test of handling for inlined code

---
+++ src/tool/hpcprof/inlines/prof-test.h
@@ -0,0 +1,10 @@
+{
+ctr++;
+#include "prof-test2.h"
+ctr++;
+}
+f();
+for(i = 0; i < N; i++) {
+	ctr++;
+}
+
---
+++ src/tool/hpcprof/inlines/prof-test2.h
@@ -0,0 +1,10 @@
+{
+ctr++;
+#include "prof-test3.h"
+ctr++;
+}
+f();
+for(i = 0; i < N; i++) {
+	ctr++;
+}
+
