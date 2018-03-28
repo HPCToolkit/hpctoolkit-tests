4c541081e0c94e4da5ad9518e003667e8309b0e3
Mark W. Krentel <krentel@rice.edu>
2018-03-28 13:52:12 -0500

    Move the functions in mixed.cpp to a separate file upper.cpp.  This makes it easier to build a separate library for all of the functions outside of the mixed.cpp main program.  Dyninst treats modules from a shared library a bit differently.

--- src/tool/hpcstruct/test1/mixed.cpp
+++ src/tool/hpcstruct/test1/mixed.cpp
@@ -10,51 +10,12 @@ extern "C" {
 long lower_half_pure_c(long, long);
 }
 
-volatile long ctr = 0;
-long N = 1;
-
-__attribute__((always_inline)) inline void n() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-__attribute__((always_inline)) inline void o() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-__attribute__((always_inline)) inline void p() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-__attribute__((always_inline)) inline void q() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-__attribute__((always_inline)) inline void r() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-__attribute__((always_inline)) inline void s() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-__attribute__((always_inline)) inline void t() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-void u() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-void v() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-void w() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-void x() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-void y() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-void z() { long i; for(i = 0; i < N; i++) { ctr++; } }
-
-long
-upper_half_c_plus_plus(long ct, long num)
-{
-    ctr = ct;
-    N = num;
-
-    n(); o(); p(); q(); r(); s(); t();
-    u(); v(); w(); x(); y(); z();
-
-    return ctr;
-}
+long upper_half_c_plus_plus(long, long);
 
 int
 main(int argc, char **argv)
 {
-    long num;
+    long ctr, num;
 
     if (argc < 2 || sscanf(argv[1], "%ld", &num) < 1) {
         num = 10000;
