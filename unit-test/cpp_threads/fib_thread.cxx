511b1fac88ef6f0ad7476a847518447443c7b7a4
Laksono Adhianto <laksono@gmail.com>
2018-06-20 21:18:43 -0500

    Add unit test for c++ std::thread

---
+++ unit-test/cpp_threads/fib_thread.cxx
@@ -0,0 +1,23 @@
+#include <thread>
+#include <future>
+
+void
+fib_thread(int n, std::promise<int> &&result) {
+  int i, j;
+  if (n<2)
+    result.set_value(1);
+  else {
+    std::promise<int> p, q;
+    auto pr = p.get_future();
+    auto pq = q.get_future();
+
+    std::thread t1(fib_thread, n-1, std::move(p));
+    std::thread t2(fib_thread, n-2, std::move(q));
+    
+    t1.join();
+    t2.join();
+
+    int tot = pr.get() + pq.get();
+    result.set_value(tot);
+  }
+}
